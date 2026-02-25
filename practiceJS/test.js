363e8e981c2dd45971caf55be94b36ad13934bf8
aparadamx <117141011+aparadamx@users.noreply.github.com>
2026-02-25 01:14:13 -0700

    others tests

---
+++ practiceJS/test.js
@@ -0,0 +1,15 @@
+const a=0;
+const b=1;
+const user="Alex";
+
+
+let data = `{a: ${a},b:${b},user: ${user}}`
+
+console.log(data);
+
+function doubleMessage(number) {
+  return `Your number double is ${2*number}`;
+
+
+}
+console.log(doubleMessage(20)); 
